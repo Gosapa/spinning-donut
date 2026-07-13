d39368dc11c101775eb0591a1464910e994ffdf1
Ian Park <ianpark0412@gmail.com>
2026-07-14 07:01:30 +0900

    feat: point structure

--- main.c
+++ main.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
 #include <raylib.h>
 
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+void DrawPoint(Point *point);
+
 int main() {
     int width = 800;
     int height = 600;
     char title[] = "My Game";
     InitWindow(width, height, title);
     SetTargetFPS(60);
-    printf("Initializing loop\n");
+
+    if (!IsWindowReady()) return 0;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("TEST", 10, 10, 20, DARKGRAY);
+        Point firstpoint = {400,300};
+        DrawPoint(&firstpoint); // Draw a point in 2d space
         EndDrawing();
-
     }
-    printf("Closing loop\n");
+
     CloseWindow();
     return 0;
+}
+
+void DrawPoint(Point *point) {
+    const int size = 10;
+    DrawRectangle(point->x, point->y, size, size, GREEN);
 }
